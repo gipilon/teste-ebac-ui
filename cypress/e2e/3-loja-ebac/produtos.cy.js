c33a00acb851c980da0d19e8a6bcc30570f813fe
Giovanna <giovanna.pilon@gmail.com>
2024-11-21 12:53:57 -0300

    testes produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,20 @@
+//// <reference type ="cypress"/>
+
+describe('Funcionalidade: produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selcionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Aero Daily Fitness Tee')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+
+    });
+});
